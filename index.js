cb232c383d0a9ea88142088fbf49db8347eddbb1
Lucas Werkmeister <mail@lucaswerkmeister.de>
2019-12-07 23:44:05 +0100

    Read environment file in index.js
    I think the EnvironmentFile in a service is not affected by its
    RootDirectory, so it doesn’t work in a portable service. Let’s read .env
    in the index.js instead. (It’s still possible to inherit the values from
    the environment instead, though I’m not sure how useful that is.)

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
+const fs = require('fs').promises;
 const process = require('process')
 const Twitter = require('twitter');
 const tweet = require('./tweet');
 
-const client = new Twitter({
-    consumer_key: process.env.TWITTER_CK,
-    consumer_secret: process.env.TWITTER_CS,
-    access_token_key: process.env.TWITTER_TK,
-    access_token_secret: process.env.TWITTER_TS,
-});
+const env = { ...process.env };
+
+async function readEnvFile(path) {
+    let contents;
+    try {
+        contents = await fs.readFile(path, 'utf8');
+    } catch (e) {
+        if (typeof e === 'object' && e.code === 'ENOENT') {
+            return;
+        } else {
+            throw e;
+        }
+    }
+    const assignments = contents.split('\n');
+    assignments.pop(); // remove trailing newline
+    for (const [name, value] of assignments.map(assignment => assignment.split('='))) {
+        env[name] = value;
+    }
+}
 
 async function sendTweet() {
+    const client = new Twitter({
+        consumer_key: env.TWITTER_CK,
+        consumer_secret: env.TWITTER_CS,
+        access_token_key: env.TWITTER_TK,
+        access_token_secret: env.TWITTER_TS,
+    });
+
     try {
         const result = await client.post('statuses/update', { status: await tweet() });
         console.log(`https://twitter.com/status/status/${result.id_str}`);
@@ -19,4 +40,4 @@ async function sendTweet() {
     }
 }
 
-sendTweet();
+readEnvFile('.env').then(sendTweet);
