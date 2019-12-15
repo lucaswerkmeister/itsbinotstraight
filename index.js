8cbd69a7720e3f0c755425595716e6e9b899372d
Lucas Werkmeister <mail@lucaswerkmeister.de>
2019-12-15 18:42:13 +0100

    Use dotenv package to read .env file

--- index.js
+++ index.js
@@ -1,34 +1,15 @@
+require('dotenv').config();
 const fs = require('fs').promises;
 const process = require('process')
 const Twitter = require('twitter');
 const tweet = require('./tweet');
 
-const env = { ...process.env };
-
-async function readEnvFile(path) {
-    let contents;
-    try {
-        contents = await fs.readFile(path, 'utf8');
-    } catch (e) {
-        if (typeof e === 'object' && e.code === 'ENOENT') {
-            return;
-        } else {
-            throw e;
-        }
-    }
-    const assignments = contents.split('\n');
-    assignments.pop(); // remove trailing newline
-    for (const [name, value] of assignments.map(assignment => assignment.split('='))) {
-        env[name] = value;
-    }
-}
-
 async function sendTweet() {
     const client = new Twitter({
-        consumer_key: env.TWITTER_CK,
-        consumer_secret: env.TWITTER_CS,
-        access_token_key: env.TWITTER_TK,
-        access_token_secret: env.TWITTER_TS,
+        consumer_key: process.env.TWITTER_CK,
+        consumer_secret: process.env.TWITTER_CS,
+        access_token_key: process.env.TWITTER_TK,
+        access_token_secret: process.env.TWITTER_TS,
     });
 
     const result = await client.post('statuses/update', { status: await tweet() });
@@ -37,7 +18,6 @@ async function sendTweet() {
 
 (async function() {
     try {
-        await readEnvFile('.env');
         await sendTweet();
     } catch (e) {
         console.error(e);
