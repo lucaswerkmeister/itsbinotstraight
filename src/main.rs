e9339fc77720f8abe7a68fda98938735a9f85dd0
Lucas Werkmeister <mail@lucaswerkmeister.de>
2020-03-08 16:06:20 +0100

    rust: Extract biword function

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,14 @@ use std::io::BufRead;
 use std::fs::File;
 use rand::seq::IteratorRandom;
 
-fn main() {
+fn biword() -> String {
     let file = File::open("biwords").expect("open biwords");
     let reader = BufReader::new(file);
     let lines = reader.lines().map(|l| l.expect("biwords lines"));
     let mut rng = rand::thread_rng();
-    println!("{}", lines.choose(&mut rng).expect("biwords empty"));
+    lines.choose(&mut rng).expect("biwords empty")
+}
+
+fn main() {
+    println!("{}", biword());
 }
