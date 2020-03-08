f6eb977c4bb080c5672af30c39c9f6e3c1d8a9e3
Lucas Werkmeister <mail@lucaswerkmeister.de>
2020-03-08 22:35:41 +0100

    rust: Keep surrounding text in replacement()
    Just replace the “bi” part of the biword and keep the rest. The JS
    version does this with regular expressions, but here we use a kind of
    linear state machine instead, manually iterating over the (decomposed)
    characters. The vowel flag isn’t used yet, which shows some warnings,
    but that’ll be taken care of separately.

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,12 @@ fn biword() -> String {
     lines.choose(&mut rng).expect("biwords empty")
 }
 
-fn replacement(biword: &str) -> &str {
+fn replacement(biword: &str) -> String {
     // ensure that we can recognize the “Bi” in “Bì…”
     let mut chars = biword.nfd();
+    // characters that occur before the “bi” (“-biotic” is a valid biword)
+    let mut before_b = String::new();
+    // whether the “b” is uppercase or not
     let b_uppercase;
     loop {
         match chars.next() {
@@ -22,12 +25,14 @@ fn replacement(biword: &str) -> &str {
                 if b == 'b' || b == 'B' {
                     b_uppercase = b == 'B';
                     break;
+                } else {
+                    before_b.push(b);
                 }
-                // TODO other chars
             },
             None => panic!("not a biword: {}", biword),
         }
     }
+    // whether the “i” is uppercase or not
     let i_uppercase;
     loop {
         match chars.next() {
@@ -35,18 +40,54 @@ fn replacement(biword: &str) -> &str {
                 if i == 'i' || i == 'I' {
                     i_uppercase = i == 'I';
                     break;
+                } else if i.is_alphanumeric() {
+                    panic!("not a biword: {}", biword);
+                } else {
+                    // could be a diacritic or something else; drop
                 }
-                // TODO other chars
             },
             None => panic!("not a biword: {}", biword),
         }
     }
-    match (b_uppercase, i_uppercase) {
+    // characters that occur after the “bi” (only as far as we’ve consumed chars)
+    let mut after_i = String::new();
+    // whether “bi” is followed by a vowel (prefer “straight”) or not (prefer “hetero”)
+    let vowel;
+    loop {
+        match chars.next() {
+            Some(c) => {
+                if c.is_alphanumeric() {
+                    if c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y' {
+                        vowel = false;
+                    } else {
+                        vowel = true;
+                    }
+                    after_i.push(c);
+                    break;
+                } else {
+                    // could be a diacritic or something else (“bi-weekly” is a valid biword);
+                    // no stdlib way to distinguish AFAIK, keep for now
+                    after_i.push(c);
+                }
+            },
+            None => {
+                vowel = false;
+                break;
+            }
+        }
+    }
+    let bi_replacement = match (b_uppercase, i_uppercase) {
         (false, false) => "straight",
         (true, false) => "Straight",
         (true, true) => "STRAIGHT",
         (false, true) => "sTrAiGhT",
-    }
+    };
+    let mut ret = String::with_capacity(biword.len() + ("straight".len() - "bi".len()));
+    ret.push_str(&before_b);
+    ret.push_str(&bi_replacement);
+    ret.push_str(&after_i);
+    ret.extend(chars);
+    ret
 }
 
 fn main() {
@@ -62,33 +103,37 @@ mod tests {
     #[test]
     fn replacement_lower_lower() {
         assert_eq!(replacement("bi"), "straight");
-        assert_eq!(replacement("bisexual"), "straight");
-        assert_eq!(replacement("-bi"), "straight");
-        assert_eq!(replacement("bì"), "straight");
+        assert_eq!(replacement("bisexual"), "straightsexual");
+        assert_eq!(replacement("-bi"), "-straight");
+        assert_eq!(replacement("bi-sexual"), "straight-sexual");
+        assert_eq!(replacement("bì"), "straight\u{300}");
         assert_eq!(replacement("ḃi"), "straight");
     }
     #[test]
     fn replacement_upper_lower() {
         assert_eq!(replacement("Bi"), "Straight");
-        assert_eq!(replacement("Bisexual"), "Straight");
-        assert_eq!(replacement("-Bi"), "Straight");
-        assert_eq!(replacement("Bì"), "Straight");
+        assert_eq!(replacement("Bisexual"), "Straightsexual");
+        assert_eq!(replacement("-Bi"), "-Straight");
+        assert_eq!(replacement("Bi-Sexual"), "Straight-Sexual");
+        assert_eq!(replacement("Bì"), "Straight\u{300}");
         assert_eq!(replacement("Ḃi"), "Straight");
     }
     #[test]
     fn replacement_lower_upper() {
         assert_eq!(replacement("bI"), "sTrAiGhT");
-        assert_eq!(replacement("bIsExUaL"), "sTrAiGhT");
-        assert_eq!(replacement("-bI"), "sTrAiGhT");
-        assert_eq!(replacement("bÌ"), "sTrAiGhT");
+        assert_eq!(replacement("bIsExUaL"), "sTrAiGhTsExUaL");
+        assert_eq!(replacement("-bI"), "-sTrAiGhT");
+        assert_eq!(replacement("bI-sExUaL"), "sTrAiGhT-sExUaL");
+        assert_eq!(replacement("bÌ"), "sTrAiGhT\u{300}");
         assert_eq!(replacement("ḃI"), "sTrAiGhT");
     }
     #[test]
     fn replacement_upper_upper() {
         assert_eq!(replacement("BI"), "STRAIGHT");
-        assert_eq!(replacement("BISEXUAL"), "STRAIGHT");
-        assert_eq!(replacement("-BI"), "STRAIGHT");
-        assert_eq!(replacement("BÌ"), "STRAIGHT");
+        assert_eq!(replacement("BISEXUAL"), "STRAIGHTSEXUAL");
+        assert_eq!(replacement("-BI"), "-STRAIGHT");
+        assert_eq!(replacement("BI-SEXUAL"), "STRAIGHT-SEXUAL");
+        assert_eq!(replacement("BÌ"), "STRAIGHT\u{300}");
         assert_eq!(replacement("ḂI"), "STRAIGHT");
     }
 
