d002c44ccd418115a542970858024f54414da3ac
Lucas Werkmeister <mail@lucaswerkmeister.de>
2020-03-13 21:55:51 +0100

    rust: Add tweet binary
    Using the dotenv library [1] to load the credentials from the .env file,
    just like the JS version, and the rust_twitter_bot_lib library [2] to
    send the tweet.
    
    This pulls in *a lot* of dependencies, and I suspect they’re coming from
    rust_twitter_bot_lib, not dotenv. Maybe I can try a different library…
    but for now, this works, so let’s commit it.
    
    [1]: https://docs.rs/dotenv/0.15.0/dotenv/
    [2]: https://docs.rs/rust_twitter_bot_lib/0.1.2/rust_twitter_bot_lib/

---
+++ src/bin/tweet.rs
@@ -0,0 +1,17 @@
+use rust_twitter_bot_lib::TwitterBot;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let bot = TwitterBot::new()
+        .consumer_key(&dotenv::var("TWITTER_CK")?)
+        .consumer_secret_key(&dotenv::var("TWITTER_CS")?)
+        .access_token(&dotenv::var("TWITTER_TK")?)
+        .secret_access_token(&dotenv::var("TWITTER_TS")?);
+
+    let biword = itsbinotstraight::biword();
+    let phrase = itsbinotstraight::phrase(&biword, &mut rand::thread_rng());
+
+    let tweet = bot.tweet(&phrase, None)?;
+    println!("https://twitter.com/status/status/{}", tweet.id());
+    Ok(())
+}
